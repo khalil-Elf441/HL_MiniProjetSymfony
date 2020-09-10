019ca4b97eb211b5404c6eca215f776a968bd3fb
KhalilEl <elfallahikhalil@gmail.com>
2020-09-11 00:42:35 +0200

    Creation des formulaires - login - inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class InscriptionController extends AbstractController
 {
